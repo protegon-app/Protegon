ffe49ab54de3c3f5ae33e1cc781ffa65670431df
guilhermehctrl07 <guilhermeh.a.s2307@gmail.com>
2025-11-30 10:47:21 -0300

    feat(auth): Salvando as alterações de UserService e Controller antes da mesclagem.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 # Protegon/__init__.py
 from flask import Flask
 
-from Protegon.controllers.auth_controller.py import auth_bp
+from Protegon.controllers.auth_controller import auth_bp
 
-def CreateApp():
+def create_app():
     app = Flask(__name__)
     
     app.register_blueprint(auth_bp, url_prefix='/api')
     
     return app
+
+app = create_app()
